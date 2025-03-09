43886fc73a947bb50adb407088833743ea46dc32
Paul3756769 <pjachmann2000@gmail.com>
2025-03-09 18:19:44 +0000

    change config to correct paths in github pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,6 +2,6 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-    site: 'https://Paul3756769.github.io/Homepage/'
+    site: 'https://paul3756769.github.io/Homepage/'
 
 });
