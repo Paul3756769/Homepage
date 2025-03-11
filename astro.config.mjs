a7fcec6f37c8bbb6ff19d077d6c7de1830032dcb
Paul <105871353+Paul3756769@users.noreply.github.com>
2025-03-11 10:04:29 +0100

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,6 +2,6 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-    site: 'https://paul3756769.github.io/',
+    site: 'https://paul3756769.github.io',
     base: "Homepage",
 });
